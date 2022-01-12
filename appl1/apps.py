7e29bd6ea23f950874a9aa54bfcf841af905e8d4
Your Name <you@example.com>
2022-01-11 23:48:10 -0800

    first commit

---
+++ appl1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Appl1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appl1'
---
+++ appl2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def xyz(request):
+    return render(request,'index2.html')
+    
